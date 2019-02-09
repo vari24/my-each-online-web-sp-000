017836e22b427744f9d82f7866b76bbe95a43fd4
Sara Varisara <varisara.n@gmail.com>
2019-02-09 08:20:24 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  n = 0
+  while n < collection.size
+  yield(collection[n])
+  n += 1
+end # put argument(s) here
+
+collection
 end
